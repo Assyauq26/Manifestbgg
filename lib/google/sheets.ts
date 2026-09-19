902a79e71867c680e5c04ae97c20b44e568ca27c
Assyauq26 <m.rifatsyauqi21@gmail.com>
2026-09-19 08:25:28 +0700

    refactor: remove direct Google Sheets client

--- lib/google/sheets.ts
+++
@@ -1,5 +0,0 @@
-import { google } from "googleapis";
-import { getGoogleAuth } from "./auth";
-export function getSheetsClient() {
-  return google.sheets({ version: "v4", auth: getGoogleAuth() });
-}
